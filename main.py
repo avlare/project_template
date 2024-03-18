b1361aabf521e0cbd899c224d2b98ee934a1188f
Anna Rechkalova <ann.rechkalova@gmail.com>
2024-03-18 13:27:34 +0200

    Modified main

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ def main():
     empty_file = "data/empty_file.txt"
     file_pandas = "data/new_table.csv"
     input_1 = input_console()
-    print(input_1)
-    input_2 = read_file(file)
-    print(input_2)
-    input_3 = read_file_pandas(file_pandas)
-    print(input_3)
     output_console(input_1)
+    input_2 = read_file(file)
+    output_console(input_2)
+    input_3 = read_file_pandas(file_pandas)
+    output_console(input_3)
     add_to_file(empty_file, input_1)
+    input_empty_file = read_file(empty_file)
+    output_console(input_empty_file)
 
 
 if __name__ == "__main__":
